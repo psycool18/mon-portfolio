fc824de46d25632f3b6fe466bc057625955f5950
Sylvain <besnard.sylvain@gmx.fr>
2024-12-19 15:00:51 +0100

    modification erreur

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-  import Header from './components/Header.vue';
-  import Footer from './components/Footer.vue';
+  import Header from './components/header.vue';
+  import Footer from './components/footer.vue';
 </script>
 
 <template>
